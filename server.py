066054123dcd76ef011d808296858f195f5a9f81
yashkins <yashkins@yandex.ru>
2021-10-01 22:48:30 +1000

    добавлена возможность сбора новостей с python.org и вывод этих новостей на нашем сервере

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from python_org_news import get_python_news
 from weather import weather_by_city
 
 app = Flask(__name__)
@@ -6,7 +7,8 @@ app = Flask(__name__)
 def index():
     page_title = 'Новости Python'
     weahter = weather_by_city('Khabarovsk,Russia')
-    return render_template('index.html',page_title=page_title,weahter=weahter)
+    news_list = get_python_news()
+    return render_template('index.html',page_title=page_title,weahter=weahter,news_list=news_list)
 
 
 if __name__ == '__main__':
