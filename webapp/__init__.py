7bc4c73283168c2c4b46daceb75153a5749ae172
yashkins <yashkins@yandex.ru>
2022-01-09 21:17:28 +1000

    в файл __init__ добавлена сортировка при получении выборки из БД. В файле index.html скорректирован вывод даты публикации - strftime('%d.%m.%Y'). В файл gitignore добавленна строка *db для скрытия БД.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -12,7 +12,7 @@ def create_app():
     def index():
         page_title = 'Новости Python'
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
-        news_list = News.query.all()
+        news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html',page_title=page_title,weather=weather,news_list=news_list)
     return app
 
--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -28,8 +28,10 @@ def get_python_news():
             save_news(title, url, published)
 
 def save_news(title, url, published):
-    new_news = News(title=title, url=url, published=published)
-    db.session.add(new_news)
-    db.session.commit()
+    news_exists = News.query.filter(News.url==url).count()
+    if not news_exists:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)
+        db.session.commit()
 
 
