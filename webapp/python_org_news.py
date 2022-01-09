7e5da757243b0db0bbbf60c4a05066b5e19b8da1
yashkins <yashkins@yandex.ru>
2022-01-09 20:50:47 +1000

    Внесены следующие изм-ия: в файл python_org_news: ф-ция get_python_news больше ничего ни возв-ет, а пишит новости БД, создана ф-ия save_news для записи в БД. Создан файл get_all_news. В файле __init__ список новостей получается не из ф-ции get_python_news, а из БД.

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+from webapp.model import db, News
 
 def get_html(url):
     try:
@@ -15,14 +17,19 @@ def get_python_news():
     if html:
         soup = BeautifulSoup(html,'html.parser')
         all_news = soup.find('ul', class_='list-recent-posts').findAll('li')
-        result_news = []
         for news in all_news:
             title = news.find('a').text
             url = news.find('a')['href']
-            date = news.find('time').text
-            result_news.append({'title':title,'url':url,'date':date})
-        return result_news
-    return False
+            published = news.find('time').text
+            try:
+                published = datetime.strptime(published,"%Y-%m-%d")
+            except ValueError:
+                published = datetime.now()
+            save_news(title, url, published)
 
+def save_news(title, url, published):
+    new_news = News(title=title, url=url, published=published)
+    db.session.add(new_news)
+    db.session.commit()
 
 
