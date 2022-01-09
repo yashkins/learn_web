e8ff6bb24fc4432f9db343fd392eb1ba422f9fb2
yashkins <yashkins@yandex.ru>
2022-01-09 18:57:20 +1000

    иправил ошибку в файле config.py. База данных создалась.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -7,6 +7,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    
     @app.route('/')
     def index():
         page_title = 'Новости Python'
