acffa029f58401553325aad13fedad2dc440465a
yashkins <yashkins@yandex.ru>
2022-01-16 22:06:59 +1000

    Создан файл create_admin.py - для создания пользователей

---
+++ create_admin.py
@@ -0,0 +1,28 @@
+from getpass import getpass
+import sys
+
+from webapp import create_app
+from webapp.model import Users, db
+
+app = create_app()
+
+with app.app_context():
+    username = input('Введите имя')
+
+    if Users.query.filter(Users.username == username).count():
+        print('Пользователь с таким имен уже существует')
+        sys.exit(0)
+    
+    password = getpass('Введите пароль')
+    password2 = getpass('Повторите пароль')
+    
+    if not password==password2:
+        print('Пароли неодинаковые')
+        sys.exit(0)
+
+    new_user = Users(username=username, role="admin")
+    new_user.set_password(password)
+
+    db.session.add(new_user)
+    db.session.commit()
+    print('Создан пользователь с id={}'.format(new_user.id))
