33fa1a3770a1164da9cafae9d939f77cc3215555
yashkins <yashkins@yandex.ru>
2022-01-21 15:55:35 +1000

    Внесены изменения: в файл __init__ добавлены route('/process-login') и route('/logout'), также добавлена фун-ия @login_manager.user_loader (и в общем включена работа с биб-текой fkask_login; в шаблоны index.html и login.html добавлен вывод сообщений, передаваемых ф-цией flash

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,13 +1,22 @@
-import re
-from flask import Flask, render_template
-from webapp.model import db, News
+
+from flask import Flask, render_template, flash, redirect, url_for
+from webapp.model import db, News, Users
 from webapp.forms import LoginForm
+from flask_login import LoginManager, login_user, logout_user   
 from webapp.weather import weather_by_city
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Users.query.get(user_id)
 
     @app.route('/')
     def index():
@@ -21,6 +30,27 @@ def create_app():
         title = 'Авторизация' 
         login_form = LoginForm()
         return render_template('login.html', page_title=title, form = login_form)   
+   
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+
+        if form.validate_on_submit():
+            user = Users.query.filter(Users.username == form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Вы успешно зашли на сайт')
+                return redirect(url_for('index'))
+
+        flash('Неверное имя пользователя или пароль')
+        return redirect(url_for('login'))
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы успешно разлогинились')
+        return redirect(url_for('index'))     
+
     return app
 
 
