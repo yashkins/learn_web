66214ba2a809ed0742e35a4d364403f23d663249
yashkins <yashkins@yandex.ru>
2022-01-28 20:09:54 +1000

    Внесены след. изменения в шаблон: создан файл base.html как основной шаблон, в каталоге templates созданы папки user и news и в них перемещены файлы login.html и index.html соответственно. Создан файл menu.html как шаблон главного меню

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -9,4 +9,4 @@ def index():
     page_title = 'Новости Python'
     weather = weather_by_city(current_app.config['WEATHER_DEFAULT_CITY'])
     news_list = News.query.order_by(News.published.desc()).all()
-    return render_template('index.html',page_title=page_title,weather=weather,news_list=news_list)
+    return render_template('news/index.html', page_title=page_title, weather=weather, news_list=news_list)
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -8,10 +8,10 @@ blueprint = Blueprint('user', __name__, url_prefix='/user')
 @blueprint.route('/login')
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('news.index'))
     title = 'Авторизация' 
     login_form = LoginForm()
-    return render_template('login.html', page_title=title, form = login_form)   
+    return render_template('user/login.html', page_title=title, form = login_form)   
 
 @blueprint.route('/process-login', methods=['POST'])
 def process_login():
