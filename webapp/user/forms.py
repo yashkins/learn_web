47dca15b6791dd78d115a34f2169f2b150a05b91
yashkins <yashkins@yandex.ru>
2022-02-03 21:20:16 +1000

    Внесены след.изм-ия: в файл webapp/user/forms.py добавлена форма для регистрации пользователя; в файл webapp/user/models.py в модель Users добавлено поле email и совершена миграция БД; создан шаблон templates/user/registration.html; в файл webapp/user/views.py добавлены route(/registration) и route(/process_reg)

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -1,9 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+from webapp.user.models import Users
 
 class LoginForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={"class": "form-control"})
     password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"class": "form-control"})
     remember_me = BooleanField('Запомнить меня', default=True, render_kw={"class": "form-check-input"})
     submit = SubmitField('Отправить!', render_kw={"class":"btn btn-primary"})
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={"class": "form-control"})
+    email = StringField('Электронная почта', validators=[DataRequired(), Email()], render_kw={"class": "form-control"})
+    password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"class": "form-control"})
+    password2 = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')], render_kw={"class": "form-control"})
+    submit = SubmitField('Отправить!', render_kw={"class":"btn btn-primary"})
+
+    def validate_username(self, username):
+        count_user = Users.query.filter_by(username=username.data).count()
+        if count_user:
+            raise ValidationError("Пользователь с таким именем уже существует")
+
+    def validate_email(self, email):
+        count_user = Users.query.filter_by(email=email.data).count()
+        if count_user:
+            raise ValidationError('Пользователь с такой эл. почтой уже зарегистрирован')
+            
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,7 +1,9 @@
+from turtle import title
 from flask import Blueprint, Flask, render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user 
+from webapp.db import db
 from webapp.user.models import Users
-from webapp.user.forms import LoginForm
+from webapp.user.forms import LoginForm, RegistrationForm
 
 blueprint = Blueprint('user', __name__, url_prefix='/user')
 
@@ -32,3 +34,28 @@ def logout():
     logout_user()
     flash('Вы успешно разлогинились')
     return redirect(url_for('news.index'))  
+
+@blueprint.route('/registration')
+def registration():
+    if current_user.is_authenticated:
+        return redirect(url_for('news.index'))
+    title = 'Регистрация'
+    registration_form = RegistrationForm()
+    return render_template('user/registration.html', page_title=title, form=registration_form)
+
+@blueprint.route('/process_reg', methods=['POST'] )
+def process_reg():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = Users(username=form.username.data, email=form.email.data, role='user')
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Вы успешно прошли регистрацию')
+        return redirect(url_for('user.login'))
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash("Ошибка в поле {}: {}".format(getattr(form,field).label.text, error))
+
+        return redirect(url_for('user.registration'))
