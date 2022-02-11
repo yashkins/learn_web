264d4e76d160939a15b380d8644ac543e16be4a7
yashkins <yashkins@yandex.ru>
2022-02-11 19:39:24 +1000

    Внесены след. изм-ия: создан файл tasks.py и добавлена работа с celery; содержимое файла  webapp/python_org_news.py перемещено в файл webapp/news/parsers/python_org_news.py, а сам файл удален, также удален файл get_all_news.py. В .gitignore добавлены расширения *.dat

--- get_all_news.py
+++
@@ -1,6 +0,0 @@
-from webapp import create_app
-from webapp.news.parsers import habr
-
-app = create_app()
-with app.app_context():
-    habr.get_news_content()
--- webapp/news/parsers/python_org_news.py
+++ webapp/news/parsers/python_org_news.py
@@ -1,17 +1,8 @@
-from datetime import datetime
-import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 from webapp.db import db
 from webapp.news.models import News
-
-def get_html(url):
-    try:
-        result = requests.get(url)
-        result.raise_for_status()
-        return result.text
-    except(requests.RequestException,ValueError):
-        print('Сетевая ошибка')
-        return False
+from utils import get_html, save_news
 
 def get_python_news():
     html = get_html('https://www.python.org/blogs/')
@@ -27,12 +18,3 @@ def get_python_news():
             except ValueError:
                 published = datetime.now()
             save_news(title, url, published)
-
-def save_news(title, url, published):
-    news_exists = News.query.filter(News.url==url).count()
-    if not news_exists:
-        new_news = News(title=title, url=url, published=published)
-        db.session.add(new_news)
-        db.session.commit()
-
-
