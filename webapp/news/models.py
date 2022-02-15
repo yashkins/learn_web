d4d0d7ffbbdc34f8ca1e1571d5b755840102abd4
yashkins <yashkins@yandex.ru>
2022-02-15 20:23:55 +1000

    Внесены след. изм-ия: в файл webapp/news/models.py добавлена модель Comments и сделана миграция + в модель News добавлена ф-ция comments_count; в шаблон webapp/templates/news/index.html добавлен подсчет коммен-ев; в шаблон webapp/templates/news/single_news.html добавлен вывод коммен-ев

--- webapp/news/models.py
+++ webapp/news/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from sqlalchemy.orm import relationship
 from webapp.db import db
 
 class News(db.Model):
@@ -6,6 +8,30 @@ class News(db.Model):
     url = db.Column(db.String, unique=True, nullable=False)
     published = db.Column(db.DateTime, nullable=False)
     text = db.Column(db.Text, nullable=True)
+
+    def comments_count(self):
+        return Comments.query.filter(Comments.news_id==self.id).count()
     
     def __repr__(self):
         return '<News {} {}>'.format(self.title, self.url)
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now())    
+    news_id = db.Column(
+        db.Integer,
+        db.ForeignKey('news.id', ondelete='CASCADE'),
+        index=True
+    )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete='CASCADE'),
+        index=True
+    )
+
+    news = relationship('News', backref='comments')
+    user = relationship('Users', backref='comments')
+
+    def repr(self):
+        return '<Comment {}>'.format(self.id)
